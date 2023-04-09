c822af2e8d3fe69ae3bc2fd633db35a6b51ae946
Akshya Iyer <91966613+AkshyaRD@users.noreply.github.com>
2023-04-09 11:50:08 +0530

    Add files via upload

---
+++ Basics_main.c
@@ -0,0 +1,15 @@
+// Basics of C 
+#include <stdio.h>
+int main()
+{
+	char name[]="SYMBIOSIS";
+	printf("%s \n",name);
+	
+	int i=10;
+	printf("%d \n",i);
+	
+	float f=8.9;
+	printf("%f \n",f);
+}
+
+
